c8664ffb1e74b65b6c7e748af125520e7eac9ab5
Baby Taz <131723655+GrayTechFun@users.noreply.github.com>
2023-08-12 13:12:01 -0400

    second-commit

---
+++ loopstudios-landing-page-main/js/index.js
@@ -0,0 +1,7 @@
+const close = document.querySelector(".close");
+const menu = document.querySelector(".menu");
+const nav = document.querySelector("nav");
+
+menu.addEventListener("click", () => {
+  nav.style.display = "block";
+});
